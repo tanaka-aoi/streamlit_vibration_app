c9c7c8128444bfc8456872a52552e198a6e36bf4
tanaka-aoi <ao-tanaka@npc21.jp>
2023-04-14 15:13:00 +0900

    first commit

--- main_app.py
+++ main_app.py
@@ -4,9 +4,12 @@ import datetime
 import pandas as pd
 # import matplotlib.pyplot as plt
 
+# st.set_page_config(layout="wide")
+
 # 基本
-st.title('vibration monitoring app')
-st.caption('設備振動値監視中')
+st.title('設備振動値監視システム')
+st.subheader('- vibration monitoring app -')
+# st.caption('設備振動値監視中')
 
 # 画像
 image = Image.open('./data/conan.png')
--- pages/graph_page.py
+++ pages/graph_page.py
@@ -2,13 +2,24 @@ import streamlit as st
 import pandas as pd
 # import matplotlib.pyplot as plt
 
+st.set_page_config(layout="wide")
+
+# サイドバー
+st.sidebar.markdown("## 設定項目")
+location_selected = st.sidebar.selectbox('監視エリア',('東京','大牟田','千綿'))
+facility_selected = st.sidebar.selectbox('監視設備',('pumpA','pumpB','pumpC'))
+item_selected = st.sidebar.multiselect('監視項目', ('変位','速度','加速度'))
+# st.sidebar.markdown("## Target Variables")
+# st.sidebar.plotly_chart(fig_target, use_container_width=True)
 
 # データ分析関連
 df = pd.read_csv('./data/data.csv', index_col='日時')
-st.dataframe(df)
-st.table(df)
+st.subheader('振動値グラフ')
+# st.table(df)
 st.line_chart(df)
-st.bar_chart(df)
+st.subheader('振動値データ')
+st.dataframe(df)
+# st.bar_chart(df)
 # st.bar_chart(df['設備A'])
 
 # matplotlib
